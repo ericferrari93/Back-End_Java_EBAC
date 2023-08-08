9ab5b14dc73a449c17cd8034ddf5781f056afca3
Eric Ferrari Ramos <ericferrarir1@gmail.com>
2023-08-08 19:43:25 -0300

    Voltando a commitar o projeto teste

---
+++ TestePrimeiroProjeto/src/br/com/eramos/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.eramos;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, Mundo!");
+	}
+
+}
